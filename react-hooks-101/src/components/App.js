2bcc8d0ebeffb0b5b1cab776bbcd9c03d928c40f
mitsuhito mondo <mitsuhito.mondo@gmail.com>
2019-09-29 18:44:59 +0900

    implement event creation

---
+++ react-hooks-101/src/components/App.js
@@ -0,0 +1,81 @@
+import React, { useReducer, useState, useEffect } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import events from '../reducers'
+
+const App = () => {
+  const [state, dispatch] = useReducer(events, []);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addEvent = e => {
+    e.preventDefault();
+
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    });
+
+    setTitle('');
+    setBody('');
+  }
+
+  console.log(state);
+
+  return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">Title</label>
+          <input 
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">Body</label>
+          <textarea 
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={e => setBody(e.target.value)}
+          />
+        </div>
+
+        <button 
+          className="btn btn-primary"
+          onClick={addEvent}
+        >
+          イベント作成する
+        </button>
+        <button 
+          className="btn btn-danger"
+        >
+          すべてのイベントを削除する
+        </button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
