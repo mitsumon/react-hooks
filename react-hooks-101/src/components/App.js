ab370c4b8d37dd41ffab3e68143e5c6d98083276
mitsuhito mondo <mitsuhito.mondo@gmail.com>
2019-09-29 18:44:59 +0900

    display events, refactor and implement remove event button

---
+++ react-hooks-101/src/components/App.js
@@ -0,0 +1,90 @@
+import React, { useReducer, useState } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Event from './Event';
+import reducer from '../reducers'
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, []);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const addEvent = e => {
+    e.preventDefault();
+
+    dispatch({
+      type: 'CREATE_EVENT',
+      title,
+      body
+    });
+
+    setTitle('');
+    setBody('');
+  }
+
+  const handleClickDeleteAll = e => {
+    e.preventDefault();
+    dispatch({
+      type: 'DELETE_ALL_EVENTS',
+    });    
+  }
+  console.log(state);
+
+  return (
+    <div className="container-fluid">
+      <h4>イベント作成フォーム</h4>
+      <form>
+        <div className="form-group">
+          <label htmlFor="formEventTitle">Title</label>
+          <input 
+            className="form-control"
+            id="formEventTitle"
+            value={title}
+            onChange={e => setTitle(e.target.value)}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="formEventBody">Body</label>
+          <textarea 
+            className="form-control"
+            id="formEventBody"
+            value={body}
+            onChange={e => setBody(e.target.value)}
+          />
+        </div>
+
+        <button 
+          className="btn btn-primary"
+          onClick={addEvent}
+        >
+          イベント作成する
+        </button>
+        <button 
+          className="btn btn-danger"
+          onClick={e => handleClickDeleteAll(e)}
+        >
+          すべてのイベントを削除する
+        </button>
+      </form>
+
+      <h4>イベント一覧</h4>
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Body</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          { state.map((event, index) => <Event key={index} event={event} dispatch={dispatch} />) }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
